73d0cdd7a7f3668d1e0cc847ca9d1a5337dab98a
Erwin Müller <erwin.mueller@deventm.org>
2017-01-09 07:02:31 +0100

    Add k8s-master debian
    see #3666

---
+++ sscontrol-osgi-k8s-master-debian/src/main/groovy/com/anrisoftware/sscontrol/k8smaster/k8smaster_1_5_debian/internal/Sshd_Debian_8.groovy
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2016 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.k8smaster.k8smaster_1_5_debian.internal
+
+import static com.anrisoftware.sscontrol.sshd.sshd_6_debian.internal.Sshd_Debian_8_Service.*
+
+import javax.inject.Inject
+
+import com.anrisoftware.propertiesutils.ContextProperties
+import com.anrisoftware.sscontrol.sshd.external.Sshd
+import com.anrisoftware.sscontrol.sshd.sshd_6_debian.external.Sshd_6_Debian
+import com.anrisoftware.sscontrol.types.external.HostServiceScriptService
+
+import groovy.util.logging.Slf4j
+
+/**
+ * Configures the <i>Sshd</i> 6 service for Debian 8.
+ *
+ * @author Erwin Müller, erwin.mueller@deventm.de
+ * @since 1.0
+ */
+@Slf4j
+class Sshd_Debian_8 extends Sshd_6_Debian {
+
+    /**
+     * 
+     *
+     * @author Erwin Müller <erwin.mueller@deventm.de>
+     * @version 1.0
+     */
+    interface Sshd_Debian_8_Factory extends HostServiceScriptService {
+    }
+
+    @Inject
+    Sshd_Debian_8_Properties debianPropertiesProvider
+
+    @Override
+    def run() {
+        setupDefaults()
+        installPackages()
+        configureService()
+        restartService()
+    }
+
+    def setupDefaults() {
+        Sshd service = service
+        if (!service.debugLogging.modules['debug']) {
+            service.debug level: defaultLogLevel
+        }
+    }
+
+    def getDefaultLogLevel() {
+        defaultProperties.getNumberProperty('default_log_level').intValue()
+    }
+
+    @Override
+    ContextProperties getDefaultProperties() {
+        debianPropertiesProvider.get()
+    }
+
+    @Override
+    Sshd getService() {
+        super.getService();
+    }
+
+    @Override
+    def getLog() {
+        log
+    }
+
+    @Override
+    String getSystemName() {
+        SYSTEM_NAME
+    }
+
+    @Override
+    String getSystemVersion() {
+        SYSTEM_VERSION
+    }
+}
---
+++ sscontrol-osgi-k8s-master-debian/src/test/groovy/com/anrisoftware/sscontrol/sshd/sshd_6_debian/internal/Sshd_Debian_8_Server_Test.groovy
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2016 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.sshd.sshd_6_debian.internal
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static com.anrisoftware.sscontrol.shell.external.utils.UnixTestUtil.*
+
+import javax.inject.Inject
+
+import org.junit.Before
+import org.junit.Test
+
+import com.anrisoftware.globalpom.strings.StringsModule
+import com.anrisoftware.globalpom.textmatch.tokentemplate.TokensTemplateModule
+import com.anrisoftware.sscontrol.debug.internal.DebugLoggingModule
+import com.anrisoftware.sscontrol.k8smaster.k8smaster_1_5_debian.internal.Sshd_Debian_8
+import com.anrisoftware.sscontrol.replace.internal.ReplaceModule
+import com.anrisoftware.sscontrol.services.internal.HostServicesModule
+import com.anrisoftware.sscontrol.shell.external.Cmd
+import com.anrisoftware.sscontrol.shell.external.utils.AbstractScriptTestBase
+import com.anrisoftware.sscontrol.shell.external.utils.SshFactory
+import com.anrisoftware.sscontrol.shell.internal.cmd.CmdModule
+import com.anrisoftware.sscontrol.shell.internal.copy.CopyModule
+import com.anrisoftware.sscontrol.shell.internal.fetch.FetchModule
+import com.anrisoftware.sscontrol.shell.internal.scp.ScpModule
+import com.anrisoftware.sscontrol.shell.internal.ssh.CmdImpl
+import com.anrisoftware.sscontrol.shell.internal.ssh.CmdRunCaller
+import com.anrisoftware.sscontrol.shell.internal.ssh.ShellCmdModule
+import com.anrisoftware.sscontrol.shell.internal.ssh.SshShellModule
+import com.anrisoftware.sscontrol.shell.internal.template.TemplateModule
+import com.anrisoftware.sscontrol.sshd.internal.SshdModule
+import com.anrisoftware.sscontrol.sshd.internal.SshdImpl.SshdImplFactory
+import com.anrisoftware.sscontrol.sshd.sshd_6_debian.internal.Sshd_Debian_8.Sshd_Debian_8_Factory
+import com.anrisoftware.sscontrol.types.external.HostServiceScript
+import com.anrisoftware.sscontrol.types.external.HostServices
+import com.anrisoftware.sscontrol.types.internal.TypesModule
+import com.google.inject.AbstractModule
+import com.google.inject.assistedinject.FactoryModuleBuilder
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+
+/**
+ * 
+ *
+ * @author Erwin Müller <erwin.mueller@deventm.de>
+ * @version 1.0
+ */
+@Slf4j
+@CompileStatic
+class Sshd_Debian_8_Server_Test extends AbstractScriptTestBase {
+
+    @Inject
+    SshdImplFactory sshdFactory
+
+    @Inject
+    Sshd_Debian_8_Factory sshdDebianFactory
+
+    @Inject
+    CmdRunCaller cmdRunCaller
+
+    @Test
+    void "sshd script"() {
+        def testCases = [
+            [
+                name: "default_target",
+                input: """
+service "sshd"
+""",
+                expected: { Map args ->
+                    File dir = args.dir as File
+                },
+            ],
+        ]
+        testCases.eachWithIndex { Map test, int k ->
+            doTest test, k
+        }
+    }
+
+    void putSshService(HostServices services) {
+        services.addService 'ssh', SshFactory.testServer(injector)
+    }
+
+    String getServiceName() {
+        'sshd'
+    }
+
+    String getScriptServiceName() {
+        'sshd/debian/8'
+    }
+
+    void createDummyCommands(File dir) {
+    }
+
+    HostServices putServices(HostServices services) {
+        services.putAvailableService 'sshd', sshdFactory
+        services.putAvailableScriptService 'sshd/debian/8', sshdDebianFactory
+    }
+
+    HostServiceScript setupScript(Map args, HostServiceScript script) {
+        return script
+    }
+
+    List getAdditionalModules() {
+        [
+            new SshdModule(),
+            new DebugLoggingModule(),
+            new TypesModule(),
+            new StringsModule(),
+            new HostServicesModule(),
+            new ShellCmdModule(),
+            new SshShellModule(),
+            new CmdModule(),
+            new ScpModule(),
+            new CopyModule(),
+            new FetchModule(),
+            new ReplaceModule(),
+            new TemplateModule(),
+            new TokensTemplateModule(),
+            new AbstractModule() {
+
+                @Override
+                protected void configure() {
+                    bind Cmd to CmdImpl
+                    install(new FactoryModuleBuilder().implement(HostServiceScript, Sshd_Debian_8).build(Sshd_Debian_8_Factory))
+                }
+            }
+        ]
+    }
+
+    @Before
+    void setupTest() {
+        toStringStyle
+        injector = createInjector()
+        injector.injectMembers(this)
+        this.threads = createThreads()
+    }
+}
