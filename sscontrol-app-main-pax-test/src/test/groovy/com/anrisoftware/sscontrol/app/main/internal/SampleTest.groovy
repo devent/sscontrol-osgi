27085d425a882d864902862d3b57a754d30b28bd
Erwin Müller <erwin.mueller@deventm.org>
2017-01-25 16:17:32 +0100

    Add Felix integration test using pax exam.

---
+++ sscontrol-app-main-pax-test/src/test/groovy/com/anrisoftware/sscontrol/app/main/internal/SampleTest.groovy
@@ -0,0 +1,45 @@
+package com.anrisoftware.sscontrol.app.main.internal
+
+import static org.ops4j.pax.exam.CoreOptions.*
+
+import javax.inject.Inject
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.ops4j.pax.exam.Configuration
+import org.ops4j.pax.exam.Option
+import org.ops4j.pax.exam.junit.PaxExam
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy
+import org.ops4j.pax.exam.spi.reactors.PerMethod
+import org.ops4j.pax.exam.util.PathUtils
+import org.osgi.framework.BundleContext
+
+import com.anrisoftware.sscontrol.types.external.HostServiceService
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public class SampleTest {
+
+    @Inject
+    BundleContext bundleContext
+
+    @Inject
+    List<HostServiceService> helloService
+
+    @Configuration
+    Option[] config() {
+        def options = []
+        options << junitBundles()
+        options << systemProperty('logback.configurationFile').value("file:${PathUtils.baseDir}/src/test/resources/logback-test.xml")
+        options << mavenBundle("org.slf4j", "slf4j-api").versionAsInProject()
+        options << mavenBundle("ch.qos.logback", "logback-classic").versionAsInProject()
+        options << mavenBundle("ch.qos.logback", "logback-core").versionAsInProject()
+        options << mavenBundle("org.codehaus.groovy", "groovy").versionAsInProject()
+        //options << bundle("reference:file:target/classes")
+    }
+
+    @Test
+    void getHelloService() {
+        helloService != null
+    }
+}
