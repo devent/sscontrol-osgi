796dc8614aff18e223494eb22815f612269a3542
Erwin Müller <erwin.mueller@deventm.org>
2017-02-05 13:55:38 +0100

    Add scripts for andrea-master.
    see #3703

---
+++ sscontrol-osgi-hosts-linux/src/test/groovy/com/anrisoftware/sscontrol/hosts/linux/internal/Hosts_Linux_Andrea_Master_Test.groovy
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.hosts.linux.internal
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static com.anrisoftware.sscontrol.shell.external.utils.UnixTestUtil.*
+
+import org.junit.Test
+
+import groovy.util.logging.Slf4j
+
+/**
+ *
+ *
+ * @author Erwin Müller <erwin.mueller@deventm.de>
+ * @version 1.0
+ */
+@Slf4j
+class Hosts_Linux_Andrea_Master_Test extends AbstractTest_Hosts_Linux {
+
+    @Test
+    void "andrea master"() {
+        def test = [
+            name: 'andrea master',
+            input: """
+service "ssh", host: "robobee@andrea-master", key: "$robobeeKey"
+service "hosts" with {
+    ip '185.24.220.41', host: 'andrea-master.muellerpublic.de', alias: 'andrea-master, etcd-0'
+}
+""",
+            expected: { Map args ->
+            },
+        ]
+        doTest test
+    }
+
+    void createDummyCommands(File dir) {
+    }
+
+    Map getScriptEnv(Map args) {
+        emptyScriptEnv
+    }
+}
