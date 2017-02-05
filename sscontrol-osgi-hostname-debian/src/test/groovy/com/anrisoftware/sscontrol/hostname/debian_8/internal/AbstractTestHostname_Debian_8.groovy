d786b79ddf64b050c29fd88fcff376d3298630c7
Erwin Müller <erwin.mueller@deventm.org>
2017-02-05 13:55:12 +0100

    Add missing module.

--- sscontrol-osgi-hostname-debian/src/test/groovy/com/anrisoftware/sscontrol/hostname/debian_8/internal/AbstractTestHostname_Debian_8.groovy
+++ sscontrol-osgi-hostname-debian/src/test/groovy/com/anrisoftware/sscontrol/hostname/debian_8/internal/AbstractTestHostname_Debian_8.groovy
@@ -43,6 +43,7 @@ import com.anrisoftware.sscontrol.shell.internal.ssh.CmdRunCaller
 import com.anrisoftware.sscontrol.shell.internal.ssh.ShellCmdModule
 import com.anrisoftware.sscontrol.shell.internal.ssh.SshShellModule
 import com.anrisoftware.sscontrol.shell.internal.template.TemplateModule
+import com.anrisoftware.sscontrol.shell.internal.templateres.TemplateResModule
 import com.anrisoftware.sscontrol.ssh.internal.SshModule
 import com.anrisoftware.sscontrol.ssh.internal.SshPreModule
 import com.anrisoftware.sscontrol.ssh.internal.SshImpl.SshImplFactory
@@ -113,6 +114,7 @@ abstract class AbstractTestHostname_Debian_8 extends AbstractScriptTestBase {
             new TemplateModule(),
             new TokensTemplateModule(),
             new FactsModule(),
+            new TemplateResModule(),
             new AbstractModule() {
 
                 @Override
