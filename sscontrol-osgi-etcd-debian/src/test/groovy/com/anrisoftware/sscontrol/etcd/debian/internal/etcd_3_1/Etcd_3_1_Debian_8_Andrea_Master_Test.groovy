a6b4a25eb86a137e458dad2cf7ed43ca4c7fc043
Erwin Müller <erwin.mueller@deventm.org>
2017-02-05 13:55:38 +0100

    Add scripts for andrea-master.
    see #3703

---
+++ sscontrol-osgi-etcd-debian/src/test/groovy/com/anrisoftware/sscontrol/etcd/debian/internal/etcd_3_1/Etcd_3_1_Debian_8_Andrea_Master_Test.groovy
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.etcd.debian.internal.etcd_3_1
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static com.anrisoftware.sscontrol.shell.external.utils.UnixTestUtil.*
+
+import org.junit.Test
+
+import groovy.util.logging.Slf4j
+
+/**
+ *
+ *
+ * @author Erwin Müller <erwin.mueller@deventm.de>
+ * @version 1.0
+ */
+@Slf4j
+class Etcd_3_1_Debian_8_Andrea_Master_Test extends AbstractTestEtcd_3_1_Debian_8 {
+
+    @Test
+    void "andrea_master"() {
+        def test = [
+            name: "andrea_master",
+            input: """
+service "ssh", host: "robobee@andrea-master", key: "$robobeeKey"
+service "etcd", member: "etcd-0" with {
+    bind scheme: "http", address: "localhost", port: 2379
+    bind scheme: "https", address: "185.24.220.41", port: 2379
+    advertise scheme: "https", address: "etcd-0.muellerpublic.de", port: 2379
+    tls cert: '$certCertPem', key: '$certKeyPem'
+}
+""",
+            expected: { Map args ->
+            },
+        ]
+        doTest test
+    }
+
+    void createDummyCommands(File dir) {
+    }
+
+    Map getScriptEnv(Map args) {
+        emptyScriptEnv
+    }
+}
---
+++ sscontrol-osgi-hostname-debian/src/test/groovy/com/anrisoftware/sscontrol/hostname/debian_8/internal/Hostname_Debian_8_Andrea_Master_Test.groovy
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.hostname.debian_8.internal
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static com.anrisoftware.sscontrol.shell.external.utils.UnixTestUtil.*
+
+import org.junit.Test
+
+import com.anrisoftware.sscontrol.types.external.HostServiceScript
+
+import groovy.util.logging.Slf4j
+
+/**
+ *
+ *
+ * @author Erwin Müller <erwin.mueller@deventm.de>
+ * @version 1.0
+ */
+@Slf4j
+class Hostname_Debian_8_Andrea_Master_Test extends AbstractTestHostname_Debian_8 {
+
+    @Test
+    void "hostname script"() {
+        def test = [
+            name: "hostname_script_fqdn",
+            input: """
+service "ssh", host: "robobee@andrea-master", key: "$robobeeKey"
+service "hostname" with {
+    // Sets the hostname.
+    set fqdn: "andrea-master.muellerpublic.de"
+}
+""",
+            expected: { Map args ->
+            },
+        ]
+        doTest test
+    }
+
+    Map getScriptEnv(Map args) {
+        emptyScriptEnv
+    }
+
+    void createDummyCommands(File dir) {
+    }
+
+    def setupServiceScript(Map args, HostServiceScript script) {
+        return script
+    }
+}
