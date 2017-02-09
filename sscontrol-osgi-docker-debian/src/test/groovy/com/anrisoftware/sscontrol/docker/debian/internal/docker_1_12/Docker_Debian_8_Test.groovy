dfa640c55ed032a12763f05c74430334da6e94ae
Erwin Müller <erwin.mueller@deventm.org>
2017-02-09 16:49:31 +0100

    Fix docker debian tests.
    see #3695

--- sscontrol-osgi-docker-debian/src/test/groovy/com/anrisoftware/sscontrol/docker/debian/internal/docker_1_12/Docker_Debian_8_Test.groovy
+++ sscontrol-osgi-docker-debian/src/test/groovy/com/anrisoftware/sscontrol/docker/debian/internal/docker_1_12/Docker_Debian_8_Test.groovy
@@ -45,7 +45,6 @@ service "docker"
                 File gen = args.test.generatedDir
                 assertFileResource Docker_Debian_8_Test, dir, "sudo.out", "${args.test.name}_sudo_expected.txt"
                 assertFileResource Docker_Debian_8_Test, dir, "apt-get.out", "${args.test.name}_apt_get_expected.txt"
-                assertFileResource Docker_Debian_8_Test, dir, "systemctl.out", "${args.test.name}_systemctl_expected.txt"
                 assertFileResource Docker_Debian_8_Test, dir, "mkdir.out", "${args.test.name}_mkdir_expected.txt"
                 assertFileResource Docker_Debian_8_Test, dir, "scp.out", "${args.test.name}_scp_expected.txt"
             },
