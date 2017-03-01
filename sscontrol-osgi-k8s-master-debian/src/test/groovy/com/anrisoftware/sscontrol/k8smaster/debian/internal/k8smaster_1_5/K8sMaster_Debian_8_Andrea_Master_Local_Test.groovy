05392a410a26874154d829dc06c4095e5c56a7dd
Erwin Müller <erwin.mueller@deventm.org>
2017-03-01 19:01:16 +0100

    Use etcd plugin address. Default protocol and port.
    finish #3714

---
+++ sscontrol-osgi-k8s-master-debian/src/test/groovy/com/anrisoftware/sscontrol/k8smaster/debian/internal/k8smaster_1_5/K8sMaster_Debian_8_Andrea_Master_Local_Test.groovy
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.k8smaster.debian.internal.k8smaster_1_5
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static com.anrisoftware.sscontrol.shell.external.utils.UnixTestUtil.*
+import static org.junit.Assume.*
+
+import org.junit.Before
+import org.junit.Test
+
+import com.anrisoftware.sscontrol.types.external.HostServices
+
+import groovy.util.logging.Slf4j
+
+/**
+ *
+ *
+ * @author Erwin Müller <erwin.mueller@deventm.de>
+ * @version 1.0
+ */
+@Slf4j
+class K8sMaster_Debian_8_Andrea_Master_Local_Test extends AbstractTest_K8sMaster_Debian_8 {
+
+    @Test
+    void "andrea_master_local"() {
+        def test = [
+            name: "andrea_master_local",
+            input: """
+service "ssh", group: "andrea-master", host: "robobee@andrea-master-local", key: "$robobeeKey"
+def andreaMaster = targets['andrea-master']
+println andreaMaster
+service "k8s-master", name: "andrea-cluster", target: andreaMaster with {
+    bind secure: "\${andreaMaster.hostAddress}"
+    tls \$certs
+    authentication "cert", ca: \$certs.ca
+    plugin "etcd", target: "etcd-0"
+    kubelet.with {
+        tls \$certs
+    }
+}
+""",
+            generatedDir: folder.newFolder(),
+            expected: { Map args ->
+            },
+        ]
+        doTest test
+    }
+
+    @Before
+    void beforeMethod() {
+        assumeTrue isHostAvailable([
+            'andrea-master-local',
+            'andrea-node-0-local'
+        ])
+    }
+
+    static final Map andreaLocalCerts = [
+        ca: AbstractTest_K8sMaster_Debian_8.class.getResource('andrea_local_k8smaster_ca_cert.pem'),
+        cert: AbstractTest_K8sMaster_Debian_8.class.getResource('andrea_local_k8smaster_robobee_test_cert.pem'),
+        key: AbstractTest_K8sMaster_Debian_8.class.getResource('andrea_local_k8smaster_robobee_test_key_insecure.pem'),
+    ]
+
+    Binding createBinding(HostServices services) {
+        Binding binding = super.createBinding(services)
+        binding.setProperty("certs", andreaLocalCerts)
+        return binding
+    }
+
+    void createDummyCommands(File dir) {
+    }
+
+    Map getScriptEnv(Map args) {
+        emptyScriptEnv
+    }
+}
