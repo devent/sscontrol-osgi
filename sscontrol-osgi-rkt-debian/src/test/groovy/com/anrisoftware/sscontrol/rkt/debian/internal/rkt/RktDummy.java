fdf77d8fd7ff63c8dc99d04d0786134a6f1a4ed0
Erwin Müller <erwin.mueller@deventm.org>
2017-03-06 07:42:33 +0100

    Add rkt modules.
    see #3717

---
+++ sscontrol-osgi-rkt-debian/src/test/groovy/com/anrisoftware/sscontrol/rkt/debian/internal/rkt/RktDummy.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.rkt.debian.internal.rkt;
+
+import static com.anrisoftware.sscontrol.types.external.StringListPropertyUtil.stringListStatement;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.codehaus.groovy.runtime.InvokerHelper;
+
+import com.anrisoftware.sscontrol.types.external.HostPropertiesService;
+import com.anrisoftware.sscontrol.types.external.HostService;
+import com.anrisoftware.sscontrol.types.external.HostServiceProperties;
+import com.anrisoftware.sscontrol.types.external.HostServiceService;
+import com.anrisoftware.sscontrol.types.external.SshHost;
+import com.anrisoftware.sscontrol.types.external.StringListPropertyUtil.ListProperty;
+import com.google.inject.assistedinject.Assisted;
+
+/**
+ * Dummy rkt script service.
+ *
+ * @author Erwin Müller, erwin.mueller@deventm.de
+ * @since 1.0
+ */
+public class RktDummy implements HostService {
+
+    /**
+     *
+     *
+     * @author Erwin Müller <erwin.mueller@deventm.de>
+     * @version 1.0
+     */
+    public interface RktDummyFactory extends HostServiceService {
+
+    }
+
+    private final List<SshHost> targets;
+
+    private final HostServiceProperties serviceProperties;
+
+    @Inject
+    RktDummy(HostPropertiesService propertiesService,
+            @Assisted Map<String, Object> args) {
+        this.targets = new ArrayList<>();
+        this.serviceProperties = propertiesService.create();
+    }
+
+    /**
+     * <pre>
+     * property << 'name=value'
+     * </pre>
+     */
+    public List<String> getProperty() {
+        return stringListStatement(new ListProperty() {
+
+            @Override
+            public void add(String property) {
+                serviceProperties.addProperty(property);
+            }
+        });
+    }
+
+    /**
+     * <pre>
+     * target name: 'master'
+     * </pre>
+     */
+    public void target(Map<String, Object> args) {
+        Object v = args.get("target");
+        @SuppressWarnings("unchecked")
+        List<SshHost> l = InvokerHelper.asList(v);
+        targets.addAll(l);
+    }
+
+    @Override
+    public SshHost getTarget() {
+        return getTargets().get(0);
+    }
+
+    @Override
+    public List<SshHost> getTargets() {
+        return Collections.unmodifiableList(targets);
+    }
+
+    @Override
+    public HostServiceProperties getServiceProperties() {
+        return serviceProperties;
+    }
+
+    @Override
+    public String getName() {
+        return "rkt";
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("name", getName())
+                .append("targets", targets).toString();
+    }
+
+}
---
+++ sscontrol-osgi-rkt-debian/src/test/groovy/com/anrisoftware/sscontrol/rkt/debian/internal/rkt_1_25_debian_8/AbstractTest_Rkt_Debian.groovy
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.rkt.debian.internal.rkt_1_25_debian_8
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static com.anrisoftware.sscontrol.shell.external.utils.UnixTestUtil.*
+
+import javax.inject.Inject
+
+import org.junit.Before
+
+import com.anrisoftware.globalpom.core.resources.ResourcesModule
+import com.anrisoftware.globalpom.core.strings.StringsModule
+import com.anrisoftware.globalpom.core.textmatch.tokentemplate.TokensTemplateModule
+import com.anrisoftware.sscontrol.debug.internal.DebugLoggingModule
+import com.anrisoftware.sscontrol.rkt.debian.internal.rkt.RktDummyModule
+import com.anrisoftware.sscontrol.rkt.debian.internal.rkt.RktDummy.RktDummyFactory
+import com.anrisoftware.sscontrol.services.internal.HostServicesModule
+import com.anrisoftware.sscontrol.shell.external.utils.AbstractScriptTestBase
+import com.anrisoftware.sscontrol.shell.internal.cmd.CmdModule
+import com.anrisoftware.sscontrol.shell.internal.copy.CopyModule
+import com.anrisoftware.sscontrol.shell.internal.facts.FactsModule
+import com.anrisoftware.sscontrol.shell.internal.fetch.FetchModule
+import com.anrisoftware.sscontrol.shell.internal.replace.ReplaceModule
+import com.anrisoftware.sscontrol.shell.internal.scp.ScpModule
+import com.anrisoftware.sscontrol.shell.internal.ssh.CmdImplModule
+import com.anrisoftware.sscontrol.shell.internal.ssh.CmdRunCaller
+import com.anrisoftware.sscontrol.shell.internal.ssh.ShellCmdModule
+import com.anrisoftware.sscontrol.shell.internal.ssh.SshShellModule
+import com.anrisoftware.sscontrol.shell.internal.template.TemplateModule
+import com.anrisoftware.sscontrol.shell.internal.templateres.TemplateResModule
+import com.anrisoftware.sscontrol.ssh.internal.SshModule
+import com.anrisoftware.sscontrol.ssh.internal.SshPreModule
+import com.anrisoftware.sscontrol.ssh.internal.SshImpl.SshImplFactory
+import com.anrisoftware.sscontrol.types.external.HostServices
+import com.anrisoftware.sscontrol.types.internal.TypesModule
+import com.google.inject.AbstractModule
+
+/**
+ *
+ *
+ * @author Erwin Müller <erwin.mueller@deventm.de>
+ * @version 1.0
+ */
+abstract class AbstractTest_Rkt_Debian extends AbstractScriptTestBase {
+
+    @Inject
+    SshImplFactory sshFactory
+
+    @Inject
+    CmdRunCaller cmdRunCaller
+
+    @Inject
+    RktDummyFactory rktDummyFactory
+
+    @Inject
+    Rkt_Debian_Factory scriptFactory
+
+    String getServiceName() {
+        'rkt-1.25'
+    }
+
+    String getScriptServiceName() {
+        'rkt-1.25/debian/8'
+    }
+
+    void createDummyCommands(File dir) {
+        createIdCommand dir
+        createEchoCommands dir, [
+            'mkdir',
+            'chown',
+            'chmod',
+            'sudo',
+            'scp',
+            'rm',
+            'cp',
+            'systemctl',
+            'which',
+            'mv',
+            'basename',
+            'wget',
+            'useradd',
+            'tar',
+            'grep',
+        ]
+    }
+
+    HostServices putServices(HostServices services) {
+        services.putAvailableService 'ssh', sshFactory
+        services.putAvailableService 'rkt-1.25', rktDummyFactory
+        services.putAvailableScriptService 'rkt-1.25/debian/8', scriptFactory
+    }
+
+    List getAdditionalModules() {
+        [
+            new SshModule(),
+            new SshPreModule(),
+            new Rkt_Debian_Module(),
+            new RktDummyModule(),
+            new DebugLoggingModule(),
+            new TypesModule(),
+            new StringsModule(),
+            new HostServicesModule(),
+            new ShellCmdModule(),
+            new SshShellModule(),
+            new CmdImplModule(),
+            new CmdModule(),
+            new ScpModule(),
+            new CopyModule(),
+            new FetchModule(),
+            new ReplaceModule(),
+            new FactsModule(),
+            new TemplateModule(),
+            new TemplateResModule(),
+            new TokensTemplateModule(),
+            new ResourcesModule(),
+            new AbstractModule() {
+
+                @Override
+                protected void configure() {
+                }
+            }
+        ]
+    }
+
+    @Before
+    void setupTest() {
+        toStringStyle
+        injector = createInjector()
+        injector.injectMembers(this)
+        this.threads = createThreads()
+    }
+}
