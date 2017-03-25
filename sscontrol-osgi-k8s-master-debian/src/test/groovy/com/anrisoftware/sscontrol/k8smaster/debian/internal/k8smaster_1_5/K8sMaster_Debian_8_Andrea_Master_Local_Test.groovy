c09353982911c17dfcac38f8a8dd9d10f59b4623
Erwin Müller <erwin.mueller@deventm.org>
2017-03-25 10:23:45 +0100

    Use kubectl to start addons.
    see #3680

--- sscontrol-osgi-k8s-master-debian/src/test/groovy/com/anrisoftware/sscontrol/k8smaster/debian/internal/k8smaster_1_5/K8sMaster_Debian_8_Andrea_Master_Local_Test.groovy
+++ sscontrol-osgi-k8s-master-debian/src/test/groovy/com/anrisoftware/sscontrol/k8smaster/debian/internal/k8smaster_1_5/K8sMaster_Debian_8_Andrea_Master_Local_Test.groovy
@@ -22,8 +22,6 @@ import static org.junit.Assume.*
 import org.junit.Before
 import org.junit.Test
 
-import com.anrisoftware.sscontrol.types.external.HostServices
-
 import groovy.util.logging.Slf4j
 
 /**
@@ -60,6 +58,7 @@ service "k8s-master", name: "andrea-cluster", target: andreaMaster, advertise: "
     }
 }
 """,
+            scriptVars: ["certs": andreaLocalCerts],
             generatedDir: folder.newFolder(),
             expected: { Map args ->
             },
@@ -87,12 +86,6 @@ service "k8s-master", name: "andrea-cluster", target: andreaMaster, advertise: "
             key: AbstractTest_K8sMaster_Debian_8.class.getResource('andrea_local_etcd_client_0_robobee_test_key_insecure.pem'),
         ]
     ]
-
-    Binding createBinding(HostServices services) {
-        Binding binding = super.createBinding(services)
-        binding.setProperty("certs", andreaLocalCerts)
-        return binding
-    }
 
     void createDummyCommands(File dir) {
     }
