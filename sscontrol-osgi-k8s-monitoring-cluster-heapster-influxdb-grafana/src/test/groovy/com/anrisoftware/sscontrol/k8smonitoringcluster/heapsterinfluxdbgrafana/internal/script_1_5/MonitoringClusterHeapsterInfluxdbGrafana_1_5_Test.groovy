5fd486f0c5900e5ef15d7a3481b9fb9aaa39298b
Erwin Müller <erwin.mueller@deventm.org>
2017-04-09 23:26:42 +0200

    Generate and apply monitoring manifests.
    see #3733

--- sscontrol-osgi-k8s-monitoring-cluster-heapster-influxdb-grafana/src/test/groovy/com/anrisoftware/sscontrol/k8smonitoringcluster/heapsterinfluxdbgrafana/internal/script_1_5/MonitoringClusterHeapsterInfluxdbGrafana_1_5_Test.groovy
+++ sscontrol-osgi-k8s-monitoring-cluster-heapster-influxdb-grafana/src/test/groovy/com/anrisoftware/sscontrol/k8smonitoringcluster/heapsterinfluxdbgrafana/internal/script_1_5/MonitoringClusterHeapsterInfluxdbGrafana_1_5_Test.groovy
@@ -37,7 +37,34 @@ class MonitoringClusterHeapsterInfluxdbGrafana_1_5_Test extends Abstract_1_5_Tes
             name: "unsecured",
             input: """
 service "ssh", host: "localhost"
-service "k8s-cluster", target: 'default', group: 'default'
+service "k8s-cluster", target: 'default'
+service "monitoring-cluster-heapster-influxdb-grafana", cluster: 'default'
+""",
+            generatedDir: folder.newFolder(),
+            expected: { Map args ->
+                File dir = args.dir
+                File gen = args.test.generatedDir
+                File binDir = new File(dir, '/usr/local/bin')
+                assertFileResource MonitoringClusterHeapsterInfluxdbGrafana_1_5_Test, binDir, "kubectl.out", "${args.test.name}_kubectl_expected.txt"
+                assertFileResource MonitoringClusterHeapsterInfluxdbGrafana_1_5_Test, new File(gen, './'), "grafana-service.yaml", "${args.test.name}_grafana_service_yaml_expected.txt"
+                assertFileResource MonitoringClusterHeapsterInfluxdbGrafana_1_5_Test, new File(gen, './'), "heapster-controller.yaml", "${args.test.name}_heapster_controller_yaml_expected.txt"
+                assertFileResource MonitoringClusterHeapsterInfluxdbGrafana_1_5_Test, new File(gen, './'), "heapster-service.yaml", "${args.test.name}_heapster_service_yaml_expected.txt"
+                assertFileResource MonitoringClusterHeapsterInfluxdbGrafana_1_5_Test, new File(gen, './'), "influxdb-grafana-controller.yaml", "${args.test.name}_influxdb_grafana_controller_yaml_expected.txt"
+                assertFileResource MonitoringClusterHeapsterInfluxdbGrafana_1_5_Test, new File(gen, './'), "influxdb-service.yaml", "${args.test.name}_influxdb_service_yaml_expected.txt"
+            },
+        ]
+        doTest test
+    }
+
+    @Test
+    void "client_cert"() {
+        def test = [
+            name: "client_cert",
+            input: """
+service "ssh", host: "localhost"
+service "k8s-cluster", target: 'default' with {
+    credentials type: 'cert', name: 'default-admin', ca: '$certCaPem', cert: '$certCertPem', key: '$certKeyPem'
+}
 service "monitoring-cluster-heapster-influxdb-grafana", cluster: 'default'
 """,
             generatedDir: folder.newFolder(),
