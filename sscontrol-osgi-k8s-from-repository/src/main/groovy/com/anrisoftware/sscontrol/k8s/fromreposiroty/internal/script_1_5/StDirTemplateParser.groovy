9031533e6057bb18a49b51227d9920a06b06239d
Erwin Müller <erwin.mueller@deventm.org>
2017-05-11 18:46:52 +0200

    Add exceptions if from-repository fails.
    see #3741

--- sscontrol-osgi-k8s-from-repository/src/main/groovy/com/anrisoftware/sscontrol/k8s/fromreposiroty/internal/script_1_5/StDirTemplateParser.groovy
+++ sscontrol-osgi-k8s-from-repository/src/main/groovy/com/anrisoftware/sscontrol/k8s/fromreposiroty/internal/script_1_5/StDirTemplateParser.groovy
@@ -1,9 +1,14 @@
 package com.anrisoftware.sscontrol.k8s.fromreposiroty.internal.script_1_5
 
+import static org.hamcrest.Matchers.*
+
 import java.nio.charset.Charset
 
 import org.apache.commons.io.FilenameUtils
 import org.stringtemplate.v4.STGroupDir
+
+import com.anrisoftware.sscontrol.k8s.fromreposiroty.external.TemplateNotFoundException
+import com.anrisoftware.sscontrol.k8s.fromreposiroty.external.TemplateParseException
 
 /**
  * Parses the file via a ST4 template engine.
@@ -19,10 +24,17 @@ class StDirTemplateParser extends AbstractTemplateParser {
     String parseFile(File parentDirectory, String fileName, Map<String, Object> args, Charset encoding) {
         def st = new STGroupDir(parentDirectory.absolutePath)
         def name = FilenameUtils.getBaseName fileName
-        def s = st.getInstanceOf(name)
-        s.add 'vars', args.vars
-        s.add 'parent', args.parent
-        return s.render()
+        try {
+            def s = st.getInstanceOf(name)
+            if (!s) {
+                throw new TemplateNotFoundException(parentDirectory, fileName, name)
+            }
+            s.add 'vars', args.vars
+            s.add 'parent', args.parent
+            return s.render()
+        } catch (e) {
+            throw new TemplateParseException(e, parentDirectory, fileName, name)
+        }
     }
 
     @Override
