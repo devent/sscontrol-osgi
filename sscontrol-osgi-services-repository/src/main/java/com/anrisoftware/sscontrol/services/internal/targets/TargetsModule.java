afc49494cbfb9cda649daf34db8bfb293bf2b6d7
Erwin Müller <erwin.mueller@deventm.org>
2017-05-27 22:27:29 +0200

    Add registries host
    see #3775

--- sscontrol-osgi-services-repository/src/main/java/com/anrisoftware/sscontrol/services/internal/targets/TargetsModule.java
+++ sscontrol-osgi-services-repository/src/main/java/com/anrisoftware/sscontrol/services/internal/targets/TargetsModule.java
@@ -17,11 +17,14 @@ package com.anrisoftware.sscontrol.services.internal.targets;
 
 import com.anrisoftware.sscontrol.services.internal.cluster.ClustersImpl;
 import com.anrisoftware.sscontrol.services.internal.cluster.ClustersImpl.ClustersImplFactory;
+import com.anrisoftware.sscontrol.services.internal.registry.RegistriesImpl;
+import com.anrisoftware.sscontrol.services.internal.registry.RegistriesImpl.RegistriesImplFactory;
 import com.anrisoftware.sscontrol.services.internal.repo.ReposImpl;
 import com.anrisoftware.sscontrol.services.internal.repo.ReposImpl.ReposImplFactory;
 import com.anrisoftware.sscontrol.services.internal.ssh.TargetsImpl;
 import com.anrisoftware.sscontrol.services.internal.ssh.TargetsImpl.TargetsImplFactory;
 import com.anrisoftware.sscontrol.types.cluster.external.Clusters;
+import com.anrisoftware.sscontrol.types.registry.external.Registries;
 import com.anrisoftware.sscontrol.types.repo.external.Repos;
 import com.anrisoftware.sscontrol.types.ssh.external.Targets;
 import com.google.inject.AbstractModule;
@@ -46,6 +49,9 @@ public class TargetsModule extends AbstractModule {
         install(new FactoryModuleBuilder()
                 .implement(Repos.class, ReposImpl.class)
                 .build(ReposImplFactory.class));
+        install(new FactoryModuleBuilder()
+                .implement(Registries.class, RegistriesImpl.class)
+                .build(RegistriesImplFactory.class));
     }
 
 }
