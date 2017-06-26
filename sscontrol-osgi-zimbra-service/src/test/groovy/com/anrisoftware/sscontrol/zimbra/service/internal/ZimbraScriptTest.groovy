d7970afd315fb3e1d01a6a634372429e13da78de
Erwin Müller <erwin.mueller@deventm.org>
2017-06-26 15:28:54 +0200

    Add Zimbra service.

---
+++ sscontrol-osgi-zimbra-service/src/test/groovy/com/anrisoftware/sscontrol/zimbra/service/internal/ZimbraScriptTest.groovy
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.zimbra.service.internal
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+
+import javax.inject.Inject
+
+import org.junit.Before
+import org.junit.Test
+
+import com.anrisoftware.propertiesutils.PropertiesUtilsModule
+import com.anrisoftware.sscontrol.properties.internal.PropertiesModule
+import com.anrisoftware.sscontrol.properties.internal.HostServicePropertiesImpl.HostServicePropertiesImplFactory
+import com.anrisoftware.sscontrol.services.internal.host.HostServicesModule
+import com.anrisoftware.sscontrol.services.internal.host.HostServicesImpl.HostServicesImplFactory
+import com.anrisoftware.sscontrol.services.internal.ssh.TargetsImpl.TargetsImplFactory
+import com.anrisoftware.sscontrol.services.internal.targets.TargetsModule
+import com.anrisoftware.sscontrol.services.internal.targets.TargetsServiceModule
+import com.anrisoftware.sscontrol.types.host.external.HostPropertiesService
+import com.anrisoftware.sscontrol.types.host.external.HostServices
+import com.anrisoftware.sscontrol.types.ssh.external.Ssh
+import com.anrisoftware.sscontrol.types.ssh.external.TargetsService
+import com.anrisoftware.sscontrol.utils.systemmappings.internal.SystemNameMappingsModule
+import com.anrisoftware.sscontrol.zimbra.service.external.Zimbra
+import com.anrisoftware.sscontrol.zimbra.service.internal.ZimbraImpl.ZimbraImplFactory
+import com.google.inject.AbstractModule
+import com.google.inject.Guice
+
+import groovy.util.logging.Slf4j
+
+/**
+ *
+ *
+ * @author Erwin Müller <erwin.mueller@deventm.de>
+ * @version 1.0
+ */
+@Slf4j
+class ZimbraScriptTest {
+
+    @Inject
+    HostServicesImplFactory servicesFactory
+
+    @Inject
+    ZimbraImplFactory hostnameFactory
+
+    @Test
+    void "zimbra"() {
+        def test = [
+            name: 'zimbra',
+            input: """
+service "zimbra", version: "8.7"
+""",
+            expected: { HostServices services ->
+                assert services.getServices('zimbra').size() == 1
+                Zimbra s = services.getServices('zimbra')[0]
+                assert s.version == '8.7'
+                assert s.name == 'zimbra-8.7'
+            },
+        ]
+        doTest test
+    }
+
+    void doTest(Map test) {
+        log.info '\n######### {} #########\ncase: {}', test.name, test
+        def services = servicesFactory.create()
+        services.targets.addTarget([getGroup: {'default'}, getHosts: { []}] as Ssh)
+        services.putAvailableService 'zimbra', hostnameFactory
+        Eval.me 'service', services, test.input as String
+        Closure expected = test.expected
+        expected services
+    }
+
+    @Before
+    void setupTest() {
+        toStringStyle
+        Guice.createInjector(
+                new ZimbraModule(),
+                new HostServicesModule(),
+                new TargetsModule(),
+                new TargetsServiceModule(),
+                new PropertiesModule(),
+                new PropertiesUtilsModule(),
+                new SystemNameMappingsModule(),
+                new AbstractModule() {
+
+                    @Override
+                    protected void configure() {
+                        bind TargetsService to TargetsImplFactory
+                        bind(HostPropertiesService).to(HostServicePropertiesImplFactory)
+                    }
+                }).injectMembers(this)
+    }
+}
