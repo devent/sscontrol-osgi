0b191e74f061af54f1d673bffda7dcf0820c8a6b
Erwin Müller <erwin.mueller@deventm.org>
2017-06-30 06:44:43 +0200

    Install collectd
    see #3831

---
+++ sscontrol-osgi-collectd-script-centos/src/main/groovy/com/anrisoftware/sscontrol/collectd/script/centos/internal/centos_7/Collectd_Centos_7.groovy
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.collectd.script.centos.internal.centos_7
+
+import static com.anrisoftware.sscontrol.collectd.script.centos.internal.centos_7.Collectd_Centos_7_Service.*
+
+import javax.inject.Inject
+
+import com.anrisoftware.propertiesutils.ContextProperties
+import com.anrisoftware.sscontrol.collectd.script.centos.external.Collectd_Centos
+
+import groovy.util.logging.Slf4j
+
+/**
+ * Collectd for CentOS 7.
+ *
+ * @author Erwin Müller, erwin.mueller@deventm.de
+ * @since 1.0
+ */
+@Slf4j
+class Collectd_Centos_7 extends Collectd_Centos {
+
+    @Inject
+    Collectd_Centos_7_Properties propertiesProvider
+
+    Collectd_5_7_Centos_7 collectd
+
+    @Inject
+    void setCollectd_5_7_Centos_7_Factory(Collectd_5_7_Centos_7_Factory factory) {
+        this.collectd = factory.create(scriptsRepository, service, target, threads, scriptEnv)
+    }
+
+    @Override
+    def run() {
+        installPackages()
+    }
+
+    @Override
+    ContextProperties getDefaultProperties() {
+        propertiesProvider.get()
+    }
+
+    @Override
+    def getLog() {
+        log
+    }
+
+    @Override
+    String getSystemName() {
+        SYSTEM_NAME
+    }
+
+    @Override
+    String getSystemVersion() {
+        SYSTEM_VERSION
+    }
+}
---
+++ sscontrol-osgi-collectd-script-centos/src/test/groovy/com/anrisoftware/sscontrol/collectd/script/centos/internal/centos_7/internal/CollectdModules.groovy
@@ -0,0 +1,74 @@
+package com.anrisoftware.sscontrol.collectd.script.centos.internal.centos_7.internal
+
+import com.anrisoftware.globalpom.core.resources.ResourcesModule
+import com.anrisoftware.globalpom.core.strings.StringsModule
+import com.anrisoftware.globalpom.core.textmatch.tokentemplate.TokensTemplateModule
+import com.anrisoftware.sscontrol.collectd.script.centos.internal.centos_7.Collectd_Centos_7_Module
+import com.anrisoftware.sscontrol.collectd.service.internal.CollectdModule
+import com.anrisoftware.sscontrol.command.shell.internal.cmd.CmdModule
+import com.anrisoftware.sscontrol.command.shell.internal.copy.CopyModule
+import com.anrisoftware.sscontrol.command.shell.internal.facts.FactsModule
+import com.anrisoftware.sscontrol.command.shell.internal.fetch.FetchModule
+import com.anrisoftware.sscontrol.command.shell.internal.replace.ReplaceModule
+import com.anrisoftware.sscontrol.command.shell.internal.scp.ScpModule
+import com.anrisoftware.sscontrol.command.shell.internal.ssh.CmdImplModule
+import com.anrisoftware.sscontrol.command.shell.internal.ssh.ShellCmdModule
+import com.anrisoftware.sscontrol.command.shell.internal.ssh.SshShellModule
+import com.anrisoftware.sscontrol.command.shell.internal.st.StModule
+import com.anrisoftware.sscontrol.command.shell.internal.template.TemplateModule
+import com.anrisoftware.sscontrol.command.shell.internal.templateres.TemplateResModule
+import com.anrisoftware.sscontrol.debug.internal.DebugLoggingModule
+import com.anrisoftware.sscontrol.services.internal.host.HostServicesModule
+import com.anrisoftware.sscontrol.ssh.internal.SshModule
+import com.anrisoftware.sscontrol.ssh.internal.SshPreModule
+import com.anrisoftware.sscontrol.types.misc.internal.TypesModule
+import com.anrisoftware.sscontrol.utils.centos.external.CentosUtilsModule
+import com.anrisoftware.sscontrol.utils.systemmappings.internal.SystemNameMappingsModule
+import com.google.inject.AbstractModule
+
+/**
+ *
+ *
+ * @author Erwin Müller <erwin.mueller@deventm.de>
+ * @version 1.0
+ */
+class CollectdModules {
+
+    /**
+     * Returns the needed modules.
+     */
+    static List getAdditionalModules() {
+        [
+            new SshModule(),
+            new SshPreModule(),
+            new CollectdModule(),
+            new Collectd_Centos_7_Module(),
+            new CentosUtilsModule(),
+            new DebugLoggingModule(),
+            new TypesModule(),
+            new StringsModule(),
+            new HostServicesModule(),
+            new ShellCmdModule(),
+            new SshShellModule(),
+            new CmdImplModule(),
+            new CmdModule(),
+            new ScpModule(),
+            new CopyModule(),
+            new FetchModule(),
+            new ReplaceModule(),
+            new FactsModule(),
+            new TemplateModule(),
+            new TemplateResModule(),
+            new TokensTemplateModule(),
+            new ResourcesModule(),
+            new SystemNameMappingsModule(),
+            new StModule(),
+            new AbstractModule() {
+
+                @Override
+                protected void configure() {
+                }
+            }
+        ]
+    }
+}
