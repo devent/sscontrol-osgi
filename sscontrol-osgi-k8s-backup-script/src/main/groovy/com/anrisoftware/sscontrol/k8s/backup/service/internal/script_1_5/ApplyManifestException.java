23e966caae414e0568f4a0850c43217ae0288915
Erwin Müller <erwin.mueller@deventm.org>
2017-07-24 16:59:36 +0200

    Add copied source.
    see #3849

---
+++ sscontrol-osgi-k8s-backup-script/src/main/groovy/com/anrisoftware/sscontrol/k8s/backup/service/internal/script_1_5/ApplyManifestException.java
@@ -0,0 +1,16 @@
+package com.anrisoftware.sscontrol.k8s.backup.service.internal.script_1_5;
+
+import java.io.File;
+
+import com.anrisoftware.sscontrol.types.app.external.AppException;
+
+@SuppressWarnings("serial")
+public class ApplyManifestException extends AppException {
+
+    public ApplyManifestException(Throwable cause, File dir, String name,
+            String content) {
+        super("Error applying manifest", cause);
+        addContextValue("file", new File(dir, name));
+        addContextValue("content", content);
+    }
+}
