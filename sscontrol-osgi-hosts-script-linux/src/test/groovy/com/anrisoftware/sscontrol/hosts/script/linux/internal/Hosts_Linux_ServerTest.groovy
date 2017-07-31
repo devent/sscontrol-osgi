8b4ec6ebb6af304d3f3b9ffe992f233a80eacaff
Erwin Müller <erwin.mueller@deventm.org>
2017-07-31 11:39:16 +0200

    Fix hosts
    Rename hosts.
    see #3855

---
+++ sscontrol-osgi-hosts-script-linux/src/test/groovy/com/anrisoftware/sscontrol/hosts/script/linux/internal/Hosts_Linux_ServerTest.groovy
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.hosts.script.linux.internal
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static com.anrisoftware.sscontrol.shell.external.utils.UnixTestUtil.*
+
+import org.junit.Test
+
+import groovy.util.logging.Slf4j
+
+/**
+ *
+ *
+ * @author Erwin Müller <erwin.mueller@deventm.de>
+ * @version 1.0
+ */
+@Slf4j
+class Hosts_Linux_ServerTest extends AbstractTest_Hosts_Linux {
+
+    @Test
+    void "test_server"() {
+        if (!isHostAvailable('robobee-test')) {
+            return
+        }
+        def test = [
+            name: 'test_server',
+            input: """
+service "ssh", host: "robobee@robobee-test", key: "$robobeeKey"
+service "hosts" with {
+    ip '127.0.0.1', host: 'localhost', alias: 'robobee-test', on: 'address'
+    ip '192.168.56.120', host: 'andrea-master.muellerpublic.de', alias: 'andrea-master'
+    ip '127.0.0.1', host: 'robobee-test.muellerpublic.de', alias: 'robobee-test'
+}
+""",
+            expected: { Map args ->
+                assertStringResource Hosts_Linux_ServerTest, readRemoteFile('/etc/hosts'), "${args.test.name}_hosts_expected.txt"
+            },
+        ]
+        doTest test
+    }
+
+    void createDummyCommands(File dir) {
+    }
+
+    Map getScriptEnv(Map args) {
+        emptyScriptEnv
+    }
+}
---
+++ sscontrol-osgi-hosts-service/src/main/java/com/anrisoftware/sscontrol/hosts/service/internal/HostsImplLogger.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.hosts.service.internal;
+
+import static com.anrisoftware.sscontrol.hosts.service.internal.HostsImplLogger.m.hostAdded;
+
+import javax.inject.Singleton;
+
+import com.anrisoftware.globalpom.log.AbstractLogger;
+import com.anrisoftware.sscontrol.hosts.service.external.Host;
+import com.anrisoftware.sscontrol.hosts.service.external.Hosts;
+
+/**
+ * Logging for {@link HostsImpl}.
+ *
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
+@Singleton
+final class HostsImplLogger extends AbstractLogger {
+
+    enum m {
+
+        hostAdded("Host {} added to {}");
+
+        private String name;
+
+        private m(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    /**
+     * Sets the context of the logger to {@link HostsImpl}.
+     */
+    public HostsImplLogger() {
+        super(HostsImpl.class);
+    }
+
+    public void hostAdded(Hosts hosts, Host h) {
+        debug(hostAdded, h, hosts);
+    }
+
+}
