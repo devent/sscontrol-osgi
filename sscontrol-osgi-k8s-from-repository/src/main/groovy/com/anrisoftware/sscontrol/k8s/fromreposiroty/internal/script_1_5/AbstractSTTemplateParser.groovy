0c24fd15d46218136dc4b5a239c93baf626f61fd
Erwin Müller <erwin.mueller@deventm.org>
2017-08-01 15:56:35 +0200

    Add url and uri base64
    see #3849

--- sscontrol-osgi-k8s-from-repository/src/main/groovy/com/anrisoftware/sscontrol/k8s/fromreposiroty/internal/script_1_5/AbstractSTTemplateParser.groovy
+++ sscontrol-osgi-k8s-from-repository/src/main/groovy/com/anrisoftware/sscontrol/k8s/fromreposiroty/internal/script_1_5/AbstractSTTemplateParser.groovy
@@ -24,6 +24,8 @@ import org.stringtemplate.v4.ST
 import org.stringtemplate.v4.STGroup
 
 import com.anrisoftware.sscontrol.utils.st.base64renderer.external.StringBase64Renderer
+import com.anrisoftware.sscontrol.utils.st.base64renderer.external.UriBase64Renderer
+import com.anrisoftware.sscontrol.utils.st.base64renderer.external.UrlBase64Renderer
 
 /**
  *
@@ -36,9 +38,17 @@ abstract class AbstractSTTemplateParser extends AbstractTemplateParser {
     @Inject
     StringBase64Renderer stringBase64Renderer
 
+    @Inject
+    UriBase64Renderer uriBase64Renderer
+
+    @Inject
+    UrlBase64Renderer urlBase64Renderer
+
     def loadTemplate(File parentDirectory, String fileName, Map<String, Object> args, Charset encoding) {
         def st = loadGroup(parentDirectory, fileName)
         st.registerRenderer stringBase64Renderer.attributeType, stringBase64Renderer
+        st.registerRenderer uriBase64Renderer.attributeType, uriBase64Renderer
+        st.registerRenderer urlBase64Renderer.attributeType, urlBase64Renderer
         def name = FilenameUtils.getBaseName fileName
         return st.getInstanceOf(name)
     }
