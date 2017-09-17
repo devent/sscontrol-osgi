d7d672d967b361d888ad922e5232373ae83f018a
Erwin Müller <erwin.mueller@deventm.org>
2017-09-17 12:56:23 +0200

    Add fail2ban cluster
    see #3886

---
+++ sscontrol-osgi-fail2ban-script-debian/src/test/groovy/com/anrisoftware/sscontrol/fail2ban/script/debian/internal/debian_9/Fail2banClusterTest.groovy
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.fail2ban.script.debian.internal.debian_9
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static com.anrisoftware.sscontrol.shell.external.utils.UnixTestUtil.*
+import static org.junit.Assume.*
+
+import org.junit.Before
+import org.junit.Test
+
+import com.anrisoftware.sscontrol.types.host.external.HostServiceScript
+
+import groovy.util.logging.Slf4j
+
+/**
+ *
+ *
+ * @author Erwin Müller <erwin.mueller@deventm.de>
+ * @version 1.0
+ */
+@Slf4j
+class Fail2banClusterTest extends AbstractFail2banRunnerTest {
+
+    @Test
+    void "server_ssh"() {
+        def test = [
+            name: "server_ssh",
+            script: '''
+service "ssh", group: "servers" with {
+    host "robobee@robobee-test", socket: "/tmp/robobee@robobee-test:22"
+    host "robobee@robobee-1-test", socket: "/tmp/robobee@robobee-1-test:22"
+}
+service "fail2ban", target: "servers" with {
+    debug "debug", level: 4
+    banning time: "PT1M"
+    jail "sshd"
+}
+''',
+            scriptVars: [:],
+            expectedServicesSize: 2,
+            expected: { Map args ->
+            },
+        ]
+        doTest test
+    }
+
+    @Before
+    void beforeMethod() {
+        assumeTrue new File('/tmp/robobee@robobee-test:22').exists()
+        assumeTrue new File('/tmp/robobee@robobee-1-test:22').exists()
+    }
+
+    Map getScriptEnv(Map args) {
+        getEmptyScriptEnv args
+    }
+
+    void createDummyCommands(File dir) {
+    }
+
+    def setupServiceScript(Map args, HostServiceScript script) {
+        return script
+    }
+}
