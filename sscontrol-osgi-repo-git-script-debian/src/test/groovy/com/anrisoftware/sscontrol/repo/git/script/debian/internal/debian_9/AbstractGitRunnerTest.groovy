1d94f853d8df3918d2ae5a3791b2aad736fec726
Erwin Müller <erwin.mueller@deventm.org>
2017-09-18 12:12:56 +0200

    Update repo-git for Debian Stretch
    finish #3888

---
+++ sscontrol-osgi-repo-git-script-debian/src/test/groovy/com/anrisoftware/sscontrol/repo/git/script/debian/internal/debian_9/AbstractGitRunnerTest.groovy
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.repo.git.script.debian.internal.debian_9
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+
+import javax.inject.Inject
+
+import org.junit.Before
+
+import com.anrisoftware.sscontrol.repo.git.service.internal.GitRepoImpl.GitRepoImplFactory
+import com.anrisoftware.sscontrol.runner.groovy.internal.RunnerModule
+import com.anrisoftware.sscontrol.runner.groovy.internal.RunScriptImpl.RunScriptImplFactory
+import com.anrisoftware.sscontrol.runner.test.external.AbstractRunnerTestBase
+import com.anrisoftware.sscontrol.ssh.script.linux.external.Ssh_Linux_Factory
+import com.anrisoftware.sscontrol.ssh.script.linux.internal.Ssh_Linux_Module
+import com.anrisoftware.sscontrol.ssh.service.internal.SshImpl.SshImplFactory
+import com.anrisoftware.sscontrol.types.host.external.HostServices
+
+/**
+ *
+ *
+ * @author Erwin Müller, erwin.mueller@deventm.de
+ * @since 1.0
+ */
+abstract class AbstractGitRunnerTest extends AbstractRunnerTestBase {
+
+    @Inject
+    RunScriptImplFactory runnerFactory
+
+    @Inject
+    SshImplFactory sshFactory
+
+    @Inject
+    Ssh_Linux_Factory sshLinuxFactory
+
+    @Inject
+    GitRepoImplFactory gitFactory
+
+    @Inject
+    GitRepoDebianFactory gitDebianFactory
+
+    def getRunScriptFactory() {
+        runnerFactory
+    }
+
+    HostServices putServices(HostServices services) {
+        services.putAvailableService 'ssh', sshFactory
+        services.putAvailableScriptService 'ssh/linux/0', sshLinuxFactory
+        services.putAvailableService 'repo-git', gitFactory
+        services.putAvailableScriptService 'repo-git/debian/9', gitDebianFactory
+        return services
+    }
+
+    List getAdditionalModules() {
+        def modules = super.additionalModules
+        modules << new RunnerModule()
+        modules << new Ssh_Linux_Module()
+        modules.addAll GitRepoTestModules.getAdditionalModules()
+        modules
+    }
+
+    @Before
+    void setupTest() {
+        toStringStyle
+        injector = createInjector()
+        injector.injectMembers(this)
+        this.threads = createThreads()
+    }
+}
---
+++ sscontrol-osgi-repo-git-script-debian/src/test/groovy/com/anrisoftware/sscontrol/repo/git/script/debian/internal/debian_9/GitRepoScriptTest.groovy
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.repo.git.script.debian.internal.debian_9
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static com.anrisoftware.sscontrol.shell.external.utils.UnixTestUtil.*
+
+import org.junit.Before
+import org.junit.Test
+
+import groovy.util.logging.Slf4j
+
+/**
+ *
+ *
+ * @author Erwin Müller <erwin.mueller@deventm.de>
+ * @version 1.0
+ */
+@Slf4j
+class GitRepoScriptTest extends AbstractGitScriptTest {
+
+    @Test
+    void "script_git_ssh"() {
+        def test = [
+            name: "script_git_ssh",
+            script: '''
+service "ssh", host: "localhost", socket: localhostSocket
+service "repo-git", group: "wordpress-app" with {
+    remote url: "git://git@github.com/user/wordpress-app"
+    credentials "ssh", key: idRsa
+}
+''',
+            scriptVars: [localhostSocket: localhostSocket, idRsa: idRsa],
+            expectedServicesSize: 2,
+            generatedDir: folder.newFolder(),
+            expected: { Map args ->
+                File dir = args.dir
+                File gen = args.test.generatedDir
+                assertFileResource GitRepoScriptTest, dir, "dpkg.out", "${args.test.name}_dpkg_expected.txt"
+                assertFileResource GitRepoScriptTest, dir, "apt-get.out", "${args.test.name}_apt_get_expected.txt"
+                assertFileResource GitRepoScriptTest, dir, "scp.out", "${args.test.name}_scp_expected.txt"
+                assertFileResource GitRepoScriptTest, dir, "mkdir.out", "${args.test.name}_mkdir_expected.txt"
+                assertFileResource GitRepoScriptTest, dir, "cp.out", "${args.test.name}_cp_expected.txt"
+                assertFileResource GitRepoScriptTest, dir, "cat.out", "${args.test.name}_cat_expected.txt"
+                assertFileResource GitRepoScriptTest, dir, "git.out", "${args.test.name}_git_expected.txt"
+            },
+        ]
+        doTest test
+    }
+
+    @Test
+    void "script_git_file"() {
+        def test = [
+            name: "script_git_file",
+            script: '''
+service "ssh", host: "localhost", socket: localhostSocket
+service "repo-git", group: 'wordpress-app' with {
+    remote url: "/user/wordpress-app.git"
+}
+''',
+            scriptVars: [localhostSocket: localhostSocket, idRsa: idRsa],
+            expectedServicesSize: 2,
+            generatedDir: folder.newFolder(),
+            expected: { Map args ->
+                File dir = args.dir
+                File gen = args.test.generatedDir
+                assertFileResource GitRepoScriptTest, dir, "git.out", "${args.test.name}_git_expected.txt"
+            },
+        ]
+        doTest test
+    }
+
+    @Before
+    void checkProfile() {
+        checkProfile LOCAL_PROFILE
+        checkLocalhostSocket()
+    }
+}
