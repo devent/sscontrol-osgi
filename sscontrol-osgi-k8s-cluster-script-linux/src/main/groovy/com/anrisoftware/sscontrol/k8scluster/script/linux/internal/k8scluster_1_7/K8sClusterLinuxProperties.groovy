84355bbb7bc4475148a260d4667353f76d47fc2d
Erwin Müller <erwin.mueller@deventm.org>
2017-09-19 16:21:12 +0200

    Fix cluster properties file.
    see #3855

--- sscontrol-osgi-k8s-cluster-script-linux/src/main/groovy/com/anrisoftware/sscontrol/k8scluster/script/linux/internal/k8scluster_1_7/K8sClusterLinuxProperties.groovy
+++ sscontrol-osgi-k8s-cluster-script-linux/src/main/groovy/com/anrisoftware/sscontrol/k8scluster/script/linux/internal/k8scluster_1_7/K8sClusterLinuxProperties.groovy
@@ -19,14 +19,14 @@ import com.anrisoftware.propertiesutils.AbstractContextPropertiesProvider
 
 /**
  * <i>K8s-Cluster properties provider from
- * {@code "/k8s_cluster_1_5_linux.properties"}.
+ * {@code "/k8s_cluster_1_7_linux.properties"}.
  *
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 1.0
  */
 class K8sClusterLinuxProperties extends AbstractContextPropertiesProvider {
 
-    private static final URL RESOURCE = K8sClusterLinuxProperties.class.getResource("/k8s_cluster_1_5_linux.properties")
+    private static final URL RESOURCE = K8sClusterLinuxProperties.class.getResource("/k8s_cluster_1_7_linux.properties")
 
     K8sClusterLinuxProperties() {
         super(K8sClusterLinuxProperties.class, RESOURCE)
