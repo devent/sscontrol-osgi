2eccae6bf3aacb43a3ee01c7c0c64109427d3781
Erwin Müller <erwin.mueller@deventm.org>
2017-09-21 17:51:01 +0200

    Update glusterfs-heketi for Debian Stretch
    see #3889

---
+++ sscontrol-osgi-k8s-glusterfs-heketi-script-debian/src/test/groovy/com/anrisoftware/sscontrol/k8s/glusterfsheketi/script/debian/internal/debian_9_script_1_7/AbstractGlusterfsHeketiRunnerTest.groovy
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.k8s.glusterfsheketi.script.debian.internal.debian_9_script_1_7
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+
+import javax.inject.Inject
+
+import org.junit.Before
+
+import com.anrisoftware.sscontrol.k8s.fromrepository.script.linux.internal.script_1_7.FromRepositoryLinuxFactory
+import com.anrisoftware.sscontrol.k8s.fromrepository.service.internal.FromRepositoryImpl.FromRepositoryImplFactory
+import com.anrisoftware.sscontrol.k8s.glusterfsheketi.service.internal.GlusterfsHeketiImpl.GlusterfsHeketiImplFactory
+import com.anrisoftware.sscontrol.k8scluster.script.linux.internal.k8scluster_1_7.K8sClusterLinuxFactory
+import com.anrisoftware.sscontrol.k8scluster.service.external.K8sClusterFactory
+import com.anrisoftware.sscontrol.registry.docker.script.linux.internal.linux.DockerRegistryLinuxFactory
+import com.anrisoftware.sscontrol.registry.docker.service.internal.DockerRegistryImpl.DockerRegistryImplFactory
+import com.anrisoftware.sscontrol.repo.git.script.debian.internal.debian_9.GitRepoDebianFactory
+import com.anrisoftware.sscontrol.repo.git.service.internal.GitRepoImpl.GitRepoImplFactory
+import com.anrisoftware.sscontrol.runner.groovy.internal.RunnerModule
+import com.anrisoftware.sscontrol.runner.groovy.internal.RunScriptImpl.RunScriptImplFactory
+import com.anrisoftware.sscontrol.runner.test.external.AbstractRunnerTestBase
+import com.anrisoftware.sscontrol.ssh.script.linux.external.Ssh_Linux_Factory
+import com.anrisoftware.sscontrol.ssh.script.linux.internal.Ssh_Linux_Module
+import com.anrisoftware.sscontrol.ssh.service.internal.SshImpl.SshImplFactory
+import com.anrisoftware.sscontrol.types.host.external.HostServices
+
+/**
+ *
+ *
+ * @author Erwin Müller, erwin.mueller@deventm.de
+ * @since 1.0
+ */
+abstract class AbstractGlusterfsHeketiRunnerTest extends AbstractRunnerTestBase {
+
+    static final Map certs = [
+        worker: [
+            ca: AbstractGlusterfsHeketiRunnerTest.class.getResource('cert_ca.txt'),
+            cert: AbstractGlusterfsHeketiRunnerTest.class.getResource('cert_cert.txt'),
+            key: AbstractGlusterfsHeketiRunnerTest.class.getResource('cert_key.txt'),
+        ],
+    ]
+
+    @Inject
+    RunScriptImplFactory runnerFactory
+
+    @Inject
+    SshImplFactory sshFactory
+
+    @Inject
+    Ssh_Linux_Factory sshLinuxFactory
+
+    @Inject
+    K8sClusterFactory clusterFactory
+
+    @Inject
+    K8sClusterLinuxFactory clusterLinuxFactory
+
+    @Inject
+    GitRepoImplFactory gitFactory
+
+    @Inject
+    GitRepoDebianFactory gitDebianFactory
+
+    @Inject
+    DockerRegistryImplFactory dockerRegistryFactory
+
+    @Inject
+    DockerRegistryLinuxFactory dockerRegistryLinuxFactory
+
+    @Inject
+    FromRepositoryImplFactory fromRepositoryFactory
+
+    @Inject
+    FromRepositoryLinuxFactory fromRepositoryLinuxFactory
+
+    @Inject
+    GlusterfsHeketiImplFactory glusterfsHeketiFactory
+
+    @Inject
+    GlusterfsHeketiDebianFactory glusterfsHeketiDebianFactory
+
+    def getRunScriptFactory() {
+        runnerFactory
+    }
+
+    HostServices putServices(HostServices services) {
+        services.putAvailableService 'ssh', sshFactory
+        services.putAvailableScriptService 'ssh/linux/0', sshLinuxFactory
+        services.putAvailableService 'k8s-cluster', clusterFactory
+        services.putAvailableScriptService 'k8s/cluster/linux/0', clusterLinuxFactory
+        services.putAvailableService 'repo-git', gitFactory
+        services.putAvailableScriptService 'repo-git/debian/9', gitDebianFactory
+        services.putAvailableService 'registry-docker', dockerRegistryFactory
+        services.putAvailableScriptService 'registry-docker/linux/0', dockerRegistryLinuxFactory
+        services.putAvailableService 'from-repository', fromRepositoryFactory
+        services.putAvailableScriptService 'from-repository/linux/0', fromRepositoryLinuxFactory
+        services.putAvailableService 'glusterfs-heketi', glusterfsHeketiFactory
+        services.putAvailableScriptService 'glusterfs-heketi/debian/9', glusterfsHeketiDebianFactory
+        return services
+    }
+
+    List getAdditionalModules() {
+        def modules = super.additionalModules
+        modules << new RunnerModule()
+        modules << new Ssh_Linux_Module()
+        modules.addAll GlusterfsHeketiTestModules.getAdditionalModules()
+        modules
+    }
+
+
+    @Before
+    void setupTest() {
+        toStringStyle
+        injector = createInjector()
+        injector.injectMembers(this)
+        this.threads = createThreads()
+    }
+}
---
+++ sscontrol-osgi-k8s-glusterfs-heketi-service/src/main/java/com/anrisoftware/sscontrol/k8s/glusterfsheketi/service/internal/GlusterfsHeketiImplLogger.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.k8s.glusterfsheketi.service.internal;
+
+import static com.anrisoftware.sscontrol.k8s.glusterfsheketi.service.internal.GlusterfsHeketiImplLogger.m.clustersAdded;
+import static com.anrisoftware.sscontrol.k8s.glusterfsheketi.service.internal.GlusterfsHeketiImplLogger.m.nodesAdded;
+import static com.anrisoftware.sscontrol.k8s.glusterfsheketi.service.internal.GlusterfsHeketiImplLogger.m.nodesSet;
+import static com.anrisoftware.sscontrol.k8s.glusterfsheketi.service.internal.GlusterfsHeketiImplLogger.m.registriesAdded;
+import static com.anrisoftware.sscontrol.k8s.glusterfsheketi.service.internal.GlusterfsHeketiImplLogger.m.reposAdded;
+import static com.anrisoftware.sscontrol.k8s.glusterfsheketi.service.internal.GlusterfsHeketiImplLogger.m.storageSet;
+
+import java.util.List;
+
+import javax.inject.Singleton;
+
+import com.anrisoftware.globalpom.log.AbstractLogger;
+import com.anrisoftware.sscontrol.k8s.glusterfsheketi.service.external.Storage;
+import com.anrisoftware.sscontrol.types.cluster.external.ClusterHost;
+import com.anrisoftware.sscontrol.types.registry.external.RegistryHost;
+import com.anrisoftware.sscontrol.types.repo.external.RepoHost;
+
+/**
+ * Logging for {@link GlusterfsHeketiImpl}.
+ *
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
+@Singleton
+final class GlusterfsHeketiImplLogger extends AbstractLogger {
+
+    enum m {
+
+        clustersAdded("Clusters {} added to {}"),
+
+        reposAdded("Repositories {} added to {}"),
+
+        registriesAdded("Repositories {} added to {}"),
+
+        nodesSet("Nodes group {} added to {}"),
+
+        nodesAdded("Nodes added to {}: {}"),
+
+        storageSet("Storage {} set for {}");
+
+        private String name;
+
+        private m(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    /**
+     * Sets the context of the logger to {@link GlusterfsHeketiImpl}.
+     */
+    GlusterfsHeketiImplLogger() {
+        super(GlusterfsHeketiImpl.class);
+    }
+
+    void clustersAdded(GlusterfsHeketiImpl g, List<ClusterHost> list) {
+        debug(clustersAdded, list, g);
+    }
+
+    void reposAdded(GlusterfsHeketiImpl g, List<RepoHost> list) {
+        debug(reposAdded, list, g);
+    }
+
+    void registriesAdded(GlusterfsHeketiImpl g, List<RegistryHost> list) {
+        debug(registriesAdded, list, g);
+    }
+
+    void nodesSet(GlusterfsHeketiImpl glusterfs, String group) {
+        debug(nodesSet, group, glusterfs);
+    }
+
+    void nodesAdded(GlusterfsHeketiImpl g, List<String> nodes) {
+        debug(nodesAdded, g, nodes);
+    }
+
+    void storageSet(GlusterfsHeketiImpl g, Storage storage) {
+        debug(storageSet, storage, g);
+    }
+}
