abbeed352947dac88b5d7441e1fef990c78d7406
Erwin Müller <erwin.mueller@deventm.org>
2017-09-27 12:45:10 +0200

    Update flanneld to v0.9
    finish #3926

---
+++ sscontrol-osgi-flannel-docker-script-debian/src/main/groovy/com/anrisoftware/sscontrol/flanneldocker/script/debian/internal/flanneldocker_0_9/debian_9/FlannelDockerDebianProperties.groovy
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.flanneldocker.script.debian.internal.flanneldocker_0_9.debian_9
+
+import com.anrisoftware.propertiesutils.AbstractContextPropertiesProvider
+
+/**
+ * Flannel-Docker properties provider from
+ * {@code "/flanneldocker_0_9_debian_9.properties"}.
+ *
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
+class FlannelDockerDebianProperties extends AbstractContextPropertiesProvider {
+
+    private static final URL RESOURCE = FlannelDockerDebianProperties.class.getResource("/flanneldocker_0_9_debian_9.properties")
+
+    FlannelDockerDebianProperties() {
+        super(FlannelDockerDebianProperties.class, RESOURCE)
+    }
+}
---
+++ sscontrol-osgi-flannel-docker-script-debian/src/test/groovy/com/anrisoftware/sscontrol/flanneldocker/script/debian/internal/flanneldocker_0_9/debian_9/FlannelDockerClusterTest.groovy
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2016-2017 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anrisoftware.sscontrol.flanneldocker.script.debian.internal.flanneldocker_0_9.debian_9
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static com.anrisoftware.sscontrol.shell.external.utils.UnixTestUtil.*
+import static org.junit.Assume.*
+
+import org.junit.Before
+import org.junit.Test
+
+import groovy.util.logging.Slf4j
+
+/**
+ *
+ *
+ * @author Erwin Müller <erwin.mueller@deventm.de>
+ * @version 1.0
+ */
+@Slf4j
+class FlannelDockerClusterTest extends AbstractFlannelDockerRunnerTest {
+
+    @Test
+    void "cluster_tls"() {
+        def test = [
+            name: "cluster_tls",
+            script: '''
+service "ssh", group: "servers" with {
+    host "robobee@robobee-test", socket: "/tmp/robobee@robobee-test:22"
+    host "robobee@robobee-1-test", socket: "/tmp/robobee@robobee-1-test:22"
+}
+service "flannel-docker", target: "servers", check: targets.servers[-1] with {
+    node << "servers"
+    bind name: "enp0s8"
+    etcd "https://10.10.10.7:22379" with {
+        tls certs
+    }
+}
+''',
+            scriptVars: [certs: robobeetestEtcdCerts],
+            expectedServicesSize: 2,
+            generatedDir: folder.newFolder(),
+            expected: { Map args ->
+            },
+        ]
+        doTest test
+    }
+
+    @Before
+    void beforeMethod() {
+        assumeTrue new File('/tmp/robobee@robobee-test:22').exists()
+        assumeTrue new File('/tmp/robobee@robobee-1-test:22').exists()
+    }
+
+    void createDummyCommands(File dir) {
+    }
+
+    Map getScriptEnv(Map args) {
+        getEmptyScriptEnv args
+    }
+}
