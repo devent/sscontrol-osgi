d2a146c76d32b6dd1479a9483a5e8f312ec88af7
Erwin Müller <erwin.mueller@deventm.org>
2018-10-16 11:46:43 +0200

    Feature #4102 Fix tests.

--- sscontrol-osgi-command-shell-openssh/src/test/groovy/com/anrisoftware/sscontrol/command/shell/internal/copy/DownloadCopyWorkerTest.groovy
+++ sscontrol-osgi-command-shell-openssh/src/test/groovy/com/anrisoftware/sscontrol/command/shell/internal/copy/DownloadCopyWorkerTest.groovy
@@ -25,9 +25,10 @@ import static org.junit.jupiter.api.Assumptions.*
 
 import javax.inject.Inject
 
-import org.junit.jupiter.api.BeforeEach
 import org.junit.Rule
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.migrationsupport.rules.EnableRuleMigrationSupport
 import org.junit.rules.TemporaryFolder
 
 import com.anrisoftware.globalpom.threads.external.core.Threads
@@ -49,6 +50,7 @@ import groovy.util.logging.Slf4j
  * @version 1.0
  */
 @Slf4j
+@EnableRuleMigrationSupport
 class DownloadCopyWorkerTest extends AbstractCmdTestBase {
 
     static Threads threads
