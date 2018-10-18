0be1c827fa8710e8735ca35c4a15c30b53124ca5
Erwin Müller <erwin.mueller@deventm.org>
2018-10-18 12:42:21 +0200

    Feature #4102 Fix tests.

--- sscontrol-osgi-k8s-cluster-script-linux/src/test/groovy/com/anrisoftware/sscontrol/k8scluster/script/linux/internal/k8scluster_1_8/ClusterScriptTest.groovy
+++ sscontrol-osgi-k8s-cluster-script-linux/src/test/groovy/com/anrisoftware/sscontrol/k8scluster/script/linux/internal/k8scluster_1_8/ClusterScriptTest.groovy
@@ -1,9 +1,34 @@
+/*-
+ * #%L
+ * sscontrol-osgi - k8s-cluster-script-linux
+ * %%
+ * Copyright (C) 2016 - 2018 Advanced Natural Research Institute
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.anrisoftware.sscontrol.k8scluster.script.linux.internal.k8scluster_1_8
 
 import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static com.anrisoftware.sscontrol.shell.external.utils.LocalhostSocketCondition.*
 import static com.anrisoftware.sscontrol.shell.external.utils.UnixTestUtil.*
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.migrationsupport.rules.EnableRuleMigrationSupport
+
+import com.anrisoftware.sscontrol.shell.external.utils.LocalhostSocketCondition
 
 import groovy.util.logging.Slf4j
 
@@ -14,6 +39,9 @@ import groovy.util.logging.Slf4j
  * @version 1.0
  */
 @Slf4j
+@EnableRuleMigrationSupport
+@EnabledIfSystemProperty(named = 'project.custom.local.tests.enabled', matches = 'true')
+@ExtendWith(LocalhostSocketCondition.class)
 class ClusterScriptTest extends AbstractClusterScriptTest {
 
     @Test
