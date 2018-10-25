8992653ee3cfce255e6bbf52c20fd79854430a34
Erwin Müller <erwin.mueller@deventm.org>
2018-10-25 11:45:04 +0200

    See #4110 Fix properties.

--- sscontrol-osgi-k8s-from-repository-script-linux/src/main/groovy/com/anrisoftware/sscontrol/k8s/fromrepository/script/linux/internal/script_1_12/FromRepositoryLinuxProperties.groovy
+++ sscontrol-osgi-k8s-from-repository-script-linux/src/main/groovy/com/anrisoftware/sscontrol/k8s/fromrepository/script/linux/internal/script_1_12/FromRepositoryLinuxProperties.groovy
@@ -23,14 +23,14 @@ import com.anrisoftware.propertiesutils.AbstractContextPropertiesProvider
 
 /**
  * From repository service for Kubernetes properties provider from
- * {@code "/from_repository_1_8_linux.properties"}.
+ * {@code "/from_repository_1_12_linux.properties"}.
  *
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 1.0
  */
 class FromRepositoryLinuxProperties extends AbstractContextPropertiesProvider {
 
-    private static final URL RESOURCE = FromRepositoryLinuxProperties.class.getResource("/from_repository_1_8_linux.properties")
+    private static final URL RESOURCE = FromRepositoryLinuxProperties.class.getResource("/from_repository_1_12_linux.properties")
 
     FromRepositoryLinuxProperties() {
         super(FromRepositoryLinuxProperties.class, RESOURCE)
