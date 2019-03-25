be70c635b17846225abd843b596d670b7b11a2c7
Erwin Müller <erwin.mueller@deventm.org>
2019-03-25 08:43:20 +0100

    See #4202 Update Helm.

--- sscontrol-osgi-k8s-from-helm-script-linux/src/test/groovy/com/anrisoftware/sscontrol/k8s/fromhelm/script/linux/internal/script_1_13/FromHelmMariaDbClusterTest.groovy
+++ sscontrol-osgi-k8s-from-helm-script-linux/src/test/groovy/com/anrisoftware/sscontrol/k8s/fromhelm/script/linux/internal/script_1_13/FromHelmMariaDbClusterTest.groovy
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,15 +40,68 @@ import groovy.util.logging.Slf4j
  */
 @Slf4j
 @ExtendWith(RobobeeSocketCondition.class)
-class FromHelmWordpressClusterTest extends AbstractFromHelmRunnerTest {
+class FromHelmMariaDbClusterTest extends AbstractFromHelmRunnerTest {
 
     @Test
-    void "cluster wordpress from chart"() {
+    void "cluster mariadb from chart"() {
         def test = [
-            name: "cluster_wordpress_chart",
+            name: "cluster_mariadb_chart",
             script: '''
 service "ssh", host: "node-0.robobee-test.test", socket: robobeeSocket
-service "from-helm", chart: "stable/mariadb" with {
+service "from-helm", chart: "stable/mariadb", version: "5.11.0" with {
+    release ns: "mariadb", name: "mariadb"
+    config << """
+image:
+  registry: docker.io
+  repository: bitnami/mariadb
+  tag: 10.1.38
+rootUser:
+  password:
+replication:
+  enabled: true
+  user: replicator
+  password:
+  forcePassword: true
+master:
+  affinity:
+    nodeAffinity:
+      requiredDuringSchedulingIgnoredDuringExecution:
+        nodeSelectorTerms:
+        - matchExpressions:
+          - key: robobeerun.com/mariadb
+            operator: In
+            values:
+            - required
+  tolerations: []
+  persistence:
+    enabled: true
+    size: 8Gi
+  resources: {}
+slave:
+  replicas: 2
+  affinity:
+    nodeAffinity:
+      requiredDuringSchedulingIgnoredDuringExecution:
+        nodeSelectorTerms:
+        - matchExpressions:
+          - key: robobeerun.com/mariadb
+            operator: In
+            values:
+            - required
+  persistence:
+    size: 8Gi
+  resources: {}
+metrics:
+  enabled: true
+  image:
+    registry: docker.io
+    repository: prom/mysqld-exporter
+    tag: v0.10.0
+  resources: {}
+  annotations:
+    prometheus.io/scrape: "true"
+prometheus.io/port: "9104"
+"""
 }
 ''',
             scriptVars: [
--- sscontrol-osgi-k8s-from-repository-script-linux/src/test/groovy/com/anrisoftware/sscontrol/k8s/fromrepository/script/linux/internal/script_1_13/FromRepositoryNfsProvisionerClusterTest.groovy
+++ sscontrol-osgi-k8s-from-repository-script-linux/src/test/groovy/com/anrisoftware/sscontrol/k8s/fromrepository/script/linux/internal/script_1_13/FromRepositoryNfsProvisionerClusterTest.groovy
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -52,7 +52,7 @@ service "k8s-cluster"
 service "repo-git", group: "nfs" with {
     remote url: "git@github.com:robobee-repos/kube-nfs-provisioner.git"
     credentials "ssh", key: robobeeKey
-    checkout branch: "v5.2.0-k8s1.13"
+    checkout branch: "release/v5.2.0-k8s1.13-r.1"
 }
 service "from-repository", repo: "nfs", dest: "/etc/kubernetes/addons/nfs" with {
     vars << [
@@ -61,7 +61,7 @@ service "from-repository", repo: "nfs", dest: "/etc/kubernetes/addons/nfs" with 
             affinity: [key: "robobeerun.com/nfs", name: "required", required: true],
             allowOnMaster: true,
             limits: [cpu: '100m', memory: '100Mi'],
-            options: [archiveOnDelete: true],
+            options: [default: true, archiveOnDelete: true],
             server: [address: "192.168.56.200", export: "/nfsfileshare/0"],
         ]
     ]
