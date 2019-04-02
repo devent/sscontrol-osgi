fe6a51ec86438adb635d5c3f555d95b8c375f013
Erwin Müller <erwin.mueller@deventm.org>
2019-04-02 16:13:00 +0200

    Add postgres cluster and keycloak.

---
+++ sscontrol-osgi-k8s-from-helm-script-linux/src/test/groovy/com/anrisoftware/sscontrol/k8s/fromhelm/script/linux/internal/script_1_13/FromHelmPostgresClusterTest.groovy
@@ -0,0 +1,147 @@
+/*-
+ * #%L
+ * sscontrol-osgi - k8s-from-helm-script-linux
+ * %%
+ * Copyright (C) 2016 - 2018 Advanced Natural Research Institute
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.anrisoftware.sscontrol.k8s.fromhelm.script.linux.internal.script_1_13
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static com.anrisoftware.sscontrol.shell.external.utils.RobobeeSocketCondition.*
+import static com.anrisoftware.sscontrol.shell.external.utils.UnixTestUtil.*
+import static org.junit.jupiter.api.Assumptions.*
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+import com.anrisoftware.sscontrol.shell.external.utils.RobobeeSocketCondition
+import com.anrisoftware.sscontrol.types.host.external.HostServiceScript
+
+import groovy.util.logging.Slf4j
+
+/**
+ *
+ *
+ * @author Erwin Müller <erwin.mueller@deventm.de>
+ * @version 1.0
+ */
+@Slf4j
+@ExtendWith(RobobeeSocketCondition.class)
+class FromHelmPostgresClusterTest extends AbstractFromHelmRunnerTest {
+
+    @Test
+    void "cluster postgres from chart"() {
+        def test = [
+            name: "cluster_postgres_chart",
+            script: '''
+service "ssh", host: "node-0.robobee-test.test", socket: robobeeSocket
+def vars = [
+  root: [password: 'oophiReighiela6choo1eiWainguliej'],
+  resources: [limits: [cpu: "0.5", memory: "200Mi"], requests: [cpu: "0.5", memory: "200Mi"]],
+]
+service "from-helm", chart: "stable/postgresql", version: "3.15.0" with {
+    release ns: "robobeerun-com-postgres", name: "postgres"
+    config << """
+image:
+  registry: docker.io
+  repository: bitnami/postgresql
+  tag: 10.7.0
+replication:
+  enabled: true
+  user: repl_user
+  password: repl_password
+  slaveReplicas: 2
+  ## Set synchronous commit mode: on, off, remote_apply, remote_write and local
+  ## ref: https://www.postgresql.org/docs/9.6/runtime-config-wal.html#GUC-WAL-LEVEL
+  synchronousCommit: "on"
+  ## From the number of `slaveReplicas` defined above, set the number of those that will have synchronous replication
+  ## NOTE: It cannot be > slaveReplicas
+  numSynchronousReplicas: 1
+  ## Replication Cluster application name. Useful for defining multiple replication policies
+  applicationName: my_application
+postgresqlUsername: postgres
+postgresqlPassword: ${vars.root.password}
+persistence:
+  enabled: true
+  size: 8Gi
+master:
+  affinity:
+    nodeAffinity:
+      requiredDuringSchedulingIgnoredDuringExecution:
+        nodeSelectorTerms:
+        - matchExpressions:
+          - key: robobeerun.com/postgres-master
+            operator: In
+            values:
+            - required
+  tolerations:
+  - effect: NoSchedule
+    key: node-role.kubernetes.io/master
+    operator: Equal
+slave:
+  affinity:
+    nodeAffinity:
+      requiredDuringSchedulingIgnoredDuringExecution:
+        nodeSelectorTerms:
+        - matchExpressions:
+          - key: robobeerun.com/postgres-slave
+            operator: In
+            values:
+            - required
+  tolerations:
+  - effect: NoSchedule
+    key: node-role.kubernetes.io/master
+    operator: Equal
+resources:
+  limits:
+    cpu: ${vars.resources.limits.cpu}
+    memory: ${vars.resources.limits.memory}
+  requests:
+    cpu: ${vars.resources.requests.cpu}
+    memory: ${vars.resources.requests.memory}
+metrics:
+  enabled: false
+  # resources: {}
+  image:
+    registry: docker.io
+    repository: wrouesnel/postgres_exporter
+    tag: v0.4.7
+"""
+}
+''',
+            scriptVars: [
+                robobeeSocket: robobeeSocket,
+                robobeeKey: robobeeKey,
+                robobeePub: robobeePub,
+            ],
+            expectedServicesSize: 2,
+            expected: { Map args ->
+            },
+        ]
+        doTest test
+    }
+
+    Map getScriptEnv(Map args) {
+        getEmptyScriptEnv args
+    }
+
+    void createDummyCommands(File dir) {
+    }
+
+    def setupServiceScript(Map args, HostServiceScript script) {
+        return script
+    }
+}
---
+++ sscontrol-osgi-k8s-from-repository-script-linux/src/test/groovy/com/anrisoftware/sscontrol/k8s/fromrepository/script/linux/internal/script_1_13/FromRepositoryKeycloakClusterTest.groovy
@@ -0,0 +1,115 @@
+/*-
+ * #%L
+ * sscontrol-osgi - k8s-from-repository-script-linux
+ * %%
+ * Copyright (C) 2016 - 2018 Advanced Natural Research Institute
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.anrisoftware.sscontrol.k8s.fromrepository.script.linux.internal.script_1_13
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static com.anrisoftware.sscontrol.shell.external.utils.RobobeeSocketCondition.*
+import static com.anrisoftware.sscontrol.shell.external.utils.UnixTestUtil.*
+import static org.junit.jupiter.api.Assumptions.*
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+import com.anrisoftware.sscontrol.shell.external.utils.RobobeeSocketCondition
+import com.anrisoftware.sscontrol.types.host.external.HostServiceScript
+
+import groovy.util.logging.Slf4j
+
+/**
+ *
+ *
+ * @author Erwin Müller <erwin.mueller@deventm.de>
+ * @version 1.0
+ */
+@Slf4j
+@ExtendWith(RobobeeSocketCondition.class)
+class FromRepositoryKeycloakClusterTest extends AbstractFromRepositoryRunnerTest {
+
+    @Test
+    void "keycloak"() {
+        def test = [
+            name: "keycloak",
+            script: '''
+service "ssh", host: "node-0.robobee-test.test", socket: robobeeSocket
+service "k8s-cluster"
+service "repo-git", group: "keycloak" with {
+    remote url: "git@github.com:robobee-repos/keycloak-deploy.git"
+    credentials "ssh", key: robobeeKey
+    checkout branch: "release/v5.0.0-r.1"
+}
+def revision = "r2"
+service "from-repository", repo: "keycloak" with {
+    vars << [
+        db: [
+            image: [name: "bitnami/postgresql", version: "10.7.0-debian-9-r48"],
+            database: "wordpressdb", user: "wordpressdb", password: "wordpressdb",
+            admin: [user: "postgres", password: "oophiReighiela6choo1eiWainguliej"],
+            host: "postgres-postgresql.robobeerun-com-postgres.svc",
+            port: 5432,
+        ]
+    ]
+    vars << [
+        keycloak: [
+            image: [name: "jboss/keycloak", version: "5.0.0"],
+            limits: [cpu: "500m", memory: "600Mi"],
+            issuer: "selfsigning-issuer",
+            revision: revision,
+            java: [heapStart: "200m", heapMax: "200m", metaspaceStart: "200m", metaspaceMax: "200m"],
+            adminUser: "admin",
+            adminPassword: "admin1234",
+            affinity: [key: "robobeerun.com/keycloak", name: "required", required: true],
+            allowOnMaster: true,
+            hosts: [
+                "keycloak.robobee.test", // main domain
+            ],
+        ]
+    ]
+    vars << [
+        pgbouncer: [
+            image: [name: "crunchydata/crunchy-pgbouncer", version: "centos7-10.7-2.3.1"],
+            limits: [cpu: "100m", memory: "100Mi"],
+            requests: [cpu: '100m', memory: '100Mi'],
+        ]
+    ]
+}
+''',
+            scriptVars: [
+                robobeeSocket: robobeeSocket,
+                robobeeKey: robobeeKey,
+                robobeePub: robobeePub,
+            ],
+            expectedServicesSize: 4,
+            expected: { Map args ->
+            },
+        ]
+        doTest test
+    }
+
+    Map getScriptEnv(Map args) {
+        getEmptyScriptEnv args
+    }
+
+    void createDummyCommands(File dir) {
+    }
+
+    def setupServiceScript(Map args, HostServiceScript script) {
+        return script
+    }
+}
