6d9cfa5f1adde65d6c2a8c45106cf3a72f5c6346
Erwin Müller <erwin.mueller@deventm.org>
2019-04-07 18:03:36 +0200

    Fix waiting for nodes
    Increase waiting timeout. For testing it could take longer than 60s.

--- sscontrol-osgi-k8s-kubectl-linux/src/main/groovy/com/anrisoftware/sscontrol/k8skubectl/linux/external/kubectl_1_13/AbstractKubectlLinux.groovy
+++ sscontrol-osgi-k8s-kubectl-linux/src/main/groovy/com/anrisoftware/sscontrol/k8skubectl/linux/external/kubectl_1_13/AbstractKubectlLinux.groovy
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -206,6 +206,7 @@ abstract class AbstractKubectlLinux extends ScriptBase {
         v.cluster = cluster
         v.resource = kubectlTemplate
         v.name = 'applyTaintsCmd'
+        v.timeout = timeoutLong
         runShell v
     }
 
@@ -225,6 +226,7 @@ abstract class AbstractKubectlLinux extends ScriptBase {
         v.cluster = cluster
         v.resource = kubectlTemplate
         v.name = 'applyLabelsCmd'
+        v.timeout = timeoutLong
         runShell v
     }
 
