d2c852738bc57e04d03586143c456668e24d5dd0
Erwin Müller <erwin.mueller@deventm.org>
2019-04-08 11:18:54 +0200

    Update license headers.

--- sscontrol-osgi-haproxy-script-debian/src/main/java/com/anrisoftware/sscontrol/haproxy/script/debian/internal/debian_9/HAProxy_Debian_9_Module.java
+++ sscontrol-osgi-haproxy-script-debian/src/main/java/com/anrisoftware/sscontrol/haproxy/script/debian/internal/debian_9/HAProxy_Debian_9_Module.java
@@ -1,28 +1,23 @@
-package com.anrisoftware.sscontrol.haproxy.script.debian.internal.debian_9;
-
-import com.anrisoftware.sscontrol.haproxy.script.debian.internal.debian_9.HAProxy_1_8_Debian_9;
-import com.anrisoftware.sscontrol.haproxy.script.debian.internal.debian_9.HAProxy_1_8_Impl;
-import com.anrisoftware.sscontrol.haproxy.script.debian.internal.debian_9.HAProxy_1_8_Ufw_Impl;
-
-/*-
- * #%L
- * sscontrol-osgi - collectd-script-centos
- * %%
- * Copyright (C) 2016 - 2018 Advanced Natural Research Institute
- * %%
+/**
+ * Copyright © 2016 Erwin Müller (erwin.mueller@anrisoftware.com)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * #L%
  */
+package com.anrisoftware.sscontrol.haproxy.script.debian.internal.debian_9;
+
+import com.anrisoftware.sscontrol.haproxy.script.debian.internal.debian_9.HAProxy_1_8_Debian_9;
+import com.anrisoftware.sscontrol.haproxy.script.debian.internal.debian_9.HAProxy_1_8_Impl;
+import com.anrisoftware.sscontrol.haproxy.script.debian.internal.debian_9.HAProxy_1_8_Ufw_Impl;
 
 import com.anrisoftware.sscontrol.types.host.external.HostServiceScript;
 import com.google.inject.AbstractModule;
